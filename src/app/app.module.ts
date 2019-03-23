592538f17075e4bd35617933d5a1be60f914952e
theerajet Chinlee <taonaja999@gmail.com>
2019-03-23 11:14:03 +0700

    new tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { PopularPage } from '../pages/popular/popular';
+import { SciencePage } from '../pages/science/science';
+import { MoviePage } from '../pages/movie/movie';
+import { DetailPage } from '../pages/detail/detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PopularPage,
+    SciencePage,
+    MoviePage,
+    DetailPage,
     TabsPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    PopularPage,
+    SciencePage,
+    MoviePage,
+    DetailPage,
     TabsPage
   ],
   providers: [
